e4ffcec79ada0519746eb206bebd04ad77632656
Alexander.Taranov <taranov.ad@phystech.edu>
2023-10-08 15:31:27 +0300

    Removed threshold as a function argument.

--- utils/compare.cpp
+++ utils/compare.cpp
@@ -1,39 +1,45 @@
-#include "../include/utils.hpp"
+#include "utils.hpp"
 
 #include <cmath>
 
-bool is_equal (const double a, const double b, const double threshold)
+#ifdef THRESHOLD
+const double threshold = THRESHOLD;
+#else 
+const double threshold = 1.e-5;
+#endif /*THRESHOLD*/
+
+bool is_equal (const double a, const double b)
 {
-        return fabs(a-b) < threshold;
+        return fabs(a - b) < threshold;
 }
 
-bool is_equal_greater (const double a, const double b, const double threshold)
+bool is_equal_greater (const double a, const double b)
 {
-        if (is_equal(a, b, threshold))
+        if (is_equal(a, b))
                 return true;
         else
                 return a > b;
 }
 
-bool is_greater (const double a, const double b, const double threshold)
+bool is_greater (const double a, const double b)
 {
-        if (is_equal(a, b, threshold))
+        if (is_equal(a, b))
                 return false;
         else
                 return a > b;
 }
 
-bool is_equal_lower (const double a, const double b, const double threshold)
+bool is_equal_lower (const double a, const double b)
 {
-        if (is_equal(a, b, threshold))
+        if (is_equal(a, b))
                 return true;
         else
                 return a < b;
 }
 
-bool is_lower (const double a, const double b, const double threshold)
+bool is_lower (const double a, const double b)
 {
-        if (is_equal(a, b, threshold))
+        if (is_equal(a, b))
                 return false;
         else
                 return a < b;
